c3208d746583e7523c7cce71f26c62e6a940b754
Святослав Деев <109512200+xkochevnikx@users.noreply.github.com>
2023-10-15 10:37:34 +0300

    fix types

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,38 @@
+type ID = string | number;
+
+interface ITodo {
+    userId: ID;
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+interface IUser {
+    name: string;
+    id: number;
+}
+
 (function () {
-    // Globals
     const todoList = document.getElementById('todo-list');
     const userSelect = document.getElementById('user-todo');
     const form = document.querySelector('form');
-    let todos = [];
-    let users = [];
+    let todos: ITodo[] = [];
+    let users: IUser[] = [];
 
-    // Attach Events
     document.addEventListener('DOMContentLoaded', initApp);
-    form.addEventListener('submit', handleSubmit);
+    form?.addEventListener('submit', handleSubmit);
 
-    // Basic Logic
-    function getUserName(userId) {
+    function getUserName(userId: ID) {
         const user = users.find((u) => u.id === userId);
-        return user.name;
+        if (user) {
+            return user.name;
+        }
     }
-    function printTodo({ id, userId, title, completed }) {
+
+    function printTodo({ id, userId, title, completed }: ITodo) {
         const li = document.createElement('li');
         li.className = 'todo-item';
-        li.dataset.id = id;
+        li.dataset.id = String(id);
         li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(
             userId
         )}</b></span>`;
@@ -36,62 +50,72 @@
         li.prepend(status);
         li.append(close);
 
-        todoList.prepend(li);
+        todoList?.prepend(li);
     }
 
-    function createUserOption(user) {
+    function createUserOption(user: IUser) {
         const option = document.createElement('option');
-        option.value = user.id;
+        option.value = String(user.id);
         option.innerText = user.name;
-
-        userSelect.append(option);
+        userSelect?.append(option);
     }
 
-    function removeTodo(todoId) {
+    function removeTodo(todoId: ID) {
         todos = todos.filter((todo) => todo.id !== todoId);
 
-        const todo = todoList.querySelector(`[data-id="${todoId}"]`);
-        todo.querySelector('input').removeEventListener(
-            'change',
-            handleTodoChange
-        );
-        todo.querySelector('.close').removeEventListener('click', handleClose);
-
-        todo.remove();
+        const todo = todoList?.querySelector(`[data-id="${todoId}"]`);
+        if (todo) {
+            todo.querySelector('input')?.removeEventListener(
+                'change',
+                handleTodoChange
+            );
+            todo.querySelector('.close')?.removeEventListener(
+                'click',
+                handleClose
+            );
+            todo.remove();
+        }
     }
 
-    function alertError(error) {
+    function alertError(error: Error) {
         alert(error.message);
     }
 
-    // Event Logic
     function initApp() {
         Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
             [todos, users] = values;
 
-            // Отправить в разметку
             todos.forEach((todo) => printTodo(todo));
             users.forEach((user) => createUserOption(user));
         });
     }
-    function handleSubmit(event) {
+
+    function handleSubmit(event: Event) {
         event.preventDefault();
-
-        createTodo({
-            userId: Number(form.user.value),
-            title: form.todo.value,
-            completed: false,
-        });
+        if (form) {
+            createTodo({
+                userId: Number(form.user.value),
+                title: form.todo.value,
+                completed: false,
+            });
+        }
     }
-    function handleTodoChange() {
-        const todoId = this.parentElement.dataset.id;
-        const completed = this.checked;
 
-        toggleTodoComplete(todoId, completed);
+    function handleTodoChange(this: HTMLInputElement) {
+        const parentElement = this.parentElement;
+        if (parentElement) {
+            const todoId = parentElement.dataset.id;
+            const completed = this.checked;
+            todoId && toggleTodoComplete(todoId, completed);
+        }
     }
-    function handleClose() {
-        const todoId = this.parentElement.dataset.id;
-        deleteTodo(todoId);
+
+    function handleClose(this: HTMLSpanElement) {
+        const parentElement = this.parentElement;
+        if (parentElement) {
+            const todoId = this.parentElement.dataset.id;
+            todoId && deleteTodo(todoId);
+        }
     }
 
     // Async logic
@@ -104,7 +128,9 @@
 
             return data;
         } catch (error) {
-            alertError(error);
+            if (error instanceof Error) {
+                alertError(error);
+            }
         }
     }
 
@@ -117,11 +143,13 @@
 
             return data;
         } catch (error) {
-            alertError(error);
+            if (error instanceof Error) {
+                alertError(error);
+            }
         }
     }
 
-    async function createTodo(todo) {
+    async function createTodo(todo: Omit<ITodo, 'id'>) {
         try {
             const response = await fetch(
                 'https://jsonplaceholder.typicode.com/todos',
@@ -138,11 +166,13 @@
 
             printTodo(newTodo);
         } catch (error) {
-            alertError(error);
+            if (error instanceof Error) {
+                alertError(error);
+            }
         }
     }
 
-    async function toggleTodoComplete(todoId, completed) {
+    async function toggleTodoComplete(todoId: ID, completed: boolean) {
         try {
             const response = await fetch(
                 `https://jsonplaceholder.typicode.com/todos/${todoId}`,
@@ -161,11 +191,13 @@
                 );
             }
         } catch (error) {
-            alertError(error);
+            if (error instanceof Error) {
+                alertError(error);
+            }
         }
     }
 
-    async function deleteTodo(todoId) {
+    async function deleteTodo(todoId: ID) {
         try {
             const response = await fetch(
                 `https://jsonplaceholder.typicode.com/todos/${todoId}`,
@@ -185,7 +217,9 @@
                 );
             }
         } catch (error) {
-            alertError(error);
+            if (error instanceof Error) {
+                alertError(error);
+            }
         }
     }
 })();
